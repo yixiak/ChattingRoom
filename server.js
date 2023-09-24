a43831c64c14c3be9778baf09fa47ba6a1d7ec56
Yixa <1257597350@qq.com>
2023-09-24 21:15:45 +0800

    connection event

--- server.js
+++ server.js
@@ -22,3 +22,63 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+var io = require('socket.io')(server)
+ // Socket.IO服务器能够监听HTTP服务器的连接，并与客户端进行实时通信
+ // 在同一端口同时处理HTTP请求以及Socket.io通信
+
+var chatRoomData=[] //用于存储以及展示聊天数据
+var connectedClients={} //使用uuid跟踪当前用户
+
+io.on('connection',(client)=>{
+  // 这一段代码都是形如
+  // client.on('Method Name', (dataSent) => {
+  //  some code that uses dataSent
+  //  })
+  // 这是Socket.io库的用法，作用是为Socket.IO客户端对象注册时间处理程序
+  // 当事件 Method Name 被触发时，会使用后面作为参数的匿名函数处理，并且将 dataSend作为参数传入
+
+  console.log("New client connected");
+
+  client.on("SendMessage",(messageData)=>{
+    chatRoomData.push(messageData)
+    sendUpdatedChatRoomData(client)
+  })
+
+  client.on("UserEnteredRoom", (userData) => {
+    var enteredRoomMessage = {message:`${userData.username} has entered the chat `,username: "", userID: 0, timeStamp: null}
+    chatRoomData.push(enteredRoomMessage)
+    sendUpdatedChatRoomData(client)
+    connectedClients[client.id]=userData
+  })
+
+  client.on("CreateUserData", ()=>{
+    let userID=uuid();
+    let username=uniqueNamesGenerator({dictionaries:[adjectives,names]});
+    var userData={userID:userID, username:username}
+    client.emit("SetUserData",userData)
+  });
+
+  client.on('disconnecting', (data)=>{
+    console.log("Client disconnecting...");
+
+    if(connectedClients[client.id]){
+      var leftRoomMessage={message:`${connectedClients[client.id].username} has left the chat`,username:"",userID:0,timeStamp:null}
+      chatRoomData.push(leftRoomMessage)
+      sendUpdatedChatRoomData(client)
+      delete connectedClients[client.id]
+    }
+  });
+
+  client.on('ClearChat', ()=>{
+    chatRoomData=[];
+    console.log(chatRoomData);
+    sendUpdatedChatRoomData(client)
+  })
+
+})
+
+function sendUpdatedChatRoomData(client){
+  client.emit("RetrieveChatRoomData",chatRoomData);
+  client.broadcast.emit("RetrieveChatRoomData",chatRoomData)
+}
